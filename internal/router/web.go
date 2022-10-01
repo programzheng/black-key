1d84c611269b141ef8810b93f53938583b889042
programzheng <programzheng@gmail.com>
2022-10-01 17:48:54 +0800

    :fire:

--- internal/router/web.go
+++ internal/router/web.go
@@ -2,18 +2,13 @@ package router
 
 import (
 	"github.com/programzheng/black-key/internal/controller/file"
-	"github.com/programzheng/black-key/internal/controller/job"
 
 	"github.com/gin-gonic/gin"
 )
 
 func setRoute(router *gin.Engine) {
-	router.GET("/jobrunner/json", job.JobJson)
 
 	// router.LoadHTMLGlob("dist/view/*")
 
-	// router.GET("/jobrunner/html", job.JobHtml)
-
 	router.GET("files/:hash_id", file.Get)
-
 }
